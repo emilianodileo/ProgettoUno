2f60471bb9f8b1317249f707341af390da5ae78d
Emiliano Dileo <emiliano.dileo76@gmail.com>
2022-06-21 17:38:41 +0200

    4 commit Progetto Uno

---
+++ test_one.py
@@ -0,0 +1,6 @@
+# il codice che vuoi informaticoare
+def somma(a, b):
+    return a + b
+# la funzione informatico che verifica l'esecuzione del codice
+def test_funzione_somma():
+    assert somma(3,3) == 7
